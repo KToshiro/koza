8fb4a8f2c775973c6d139612b42cccae48649a26
KToshiro <digisuppo.com@gmail.com>
2016-12-18 20:57:19 +0900

    5.5_Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'users/new'
+
   root 'static_pages#home'
+  get  '/news',    to: 'static_pages#news'
+  get  '/about',   to: 'static_pages#about'
+  get  '/help',    to: 'static_pages#help'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 
-  get 'static_pages/home'
-
-  get 'static_pages/news'
-
-  get 'static_pages/about'
-
-  get 'static_pages/help'
-
-  get 'static_pages/contact'
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,31 +12,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "The Programing Skill Up KOZA"
   end
 
   test "should get news" do
-    get static_pages_news_url
+    get news_path
     assert_response :success
     assert_select "title", "News | The Programing Skill Up KOZA"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | The Programing Skill Up KOZA"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | The Programing Skill Up KOZA"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | The Programing Skill Up KOZA"
   end
